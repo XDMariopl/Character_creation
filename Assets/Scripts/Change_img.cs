32da6a9de1cf30f9a5ab23a729703cfb55c8ece2
Maris Klaiva(Home) <maris.klaiva@gmail.com>
2023-05-09 21:48:39 +0300

    More outfits

--- Assets/Scripts/Change_img.cs
+++ Assets/Scripts/Change_img.cs
@@ -45,27 +45,55 @@ public class Change_img : MonoBehaviour {
         VariableImg.GetComponent<Image>().sprite = CharMas[1];
     }
     
-    public void HatStyles(bool HatOnOff)
+    public void HatActive(bool HatOnOff)
     {
         HatDropdown.GetComponent<Dropdown>().interactable=HatOnOff;
         HatImg.gameObject.SetActive(HatOnOff);
         
     }
 
-    public void ShirtStyles(bool ShirtOnOff)
+    public void ShirtActive(bool ShirtOnOff)
     {
         ShirtDropdown.GetComponent<Dropdown>().interactable=ShirtOnOff;
         ShirtImg.gameObject.SetActive(ShirtOnOff);
     }
-    public void PantsStyles(bool PantsOnOff)
+    public void PantsActive(bool PantsOnOff)
     {
         PantsDropdown.GetComponent<Dropdown>().interactable=PantsOnOff;
         PantsImg.gameObject.SetActive(PantsOnOff);
     }
-    public void ShoesStyles(bool ShoesOnOff)
+    public void ShoesActive(bool ShoesOnOff)
     {
         ShoesDropdown.GetComponent<Dropdown>().interactable=ShoesOnOff;
         ShoesImg.gameObject.SetActive(ShoesOnOff);
+    }
+
+    public void HatStyles(int number)
+    {
+        if(number==0) { HatImg.GetComponent<Image>().sprite = HatMas[0]; }
+        if (number == 1) { HatImg.GetComponent<Image>().sprite= HatMas[1]; }
+        if (number == 2) { HatImg.GetComponent<Image>().sprite = HatMas[2]; }
+    }
+
+    public void ShirtStyles(int number)
+    {
+        if (number == 0) { ShirtImg.GetComponent<Image>().sprite = ShirtMas[0]; }
+        if (number == 1) { ShirtImg.GetComponent<Image>().sprite = ShirtMas[1]; }
+        if (number == 2) { ShirtImg.GetComponent<Image>().sprite = ShirtMas[2]; }
+    }
+
+    public void PantsStyles(int number)
+    {
+        if (number == 0) { PantsImg.GetComponent<Image>().sprite = PantsMas[0]; }
+        if (number == 1) { PantsImg.GetComponent<Image>().sprite = PantsMas[1]; }
+        if (number == 2) { PantsImg.GetComponent<Image>().sprite = PantsMas[2]; }
+    }
+
+    public void ShoesStyles(int number)
+    {
+        if (number == 0) { ShoesImg.GetComponent<Image>().sprite = ShoesMas[0]; }
+        if (number == 1) { ShoesImg.GetComponent<Image>().sprite = ShoesMas[1]; }
+        if (number == 2) { ShoesImg.GetComponent<Image>().sprite = ShoesMas[2]; }
     }
 
 }
